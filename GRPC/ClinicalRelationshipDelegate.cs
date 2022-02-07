a436413c9aa66be93d7209947dd0642732d7ddbf
AJStangl <astangl@leaporbit.com>
2022-02-07 09:35:42 -0500

    Merging stuff

--- GRPC/ClinicalRelationshipDelegate.cs
+++ GRPC/ClinicalRelationshipDelegate.cs
@@ -25,8 +25,7 @@ namespace ProtoApp.GRPC
         private readonly ILogger<ClinicalRelationshipDelegate> _logger;
         private readonly PatientRelationshipContext _context;
 
-        public ClinicalRelationshipDelegate(ILogger<ClinicalRelationshipDelegate> logger,
-            PatientRelationshipContext context)
+        public ClinicalRelationshipDelegate(ILogger<ClinicalRelationshipDelegate> logger, PatientRelationshipContext context)
         {
             _logger = logger;
             _context = context;
@@ -35,26 +34,16 @@ namespace ProtoApp.GRPC
         /// <inheritdoc />
         public async Task<ClinicalRelationshipResponse> Handle(ClinicalRelationshipRequest clinicalRelationshipRequest)
         {
-            /*TODO: Query for Organization based on patient
-             * select * from Patient p
-                inner join Relationship r on p.Id = r.PatientId inner join Organization org on r.Id = org.RelationshipId
-                left join Address a on org.AddressId = a.Id
-                left join Contact C on C.Id = org.ContactId
-                left join OrganizationProgram OP on org.Id = OP.OrganizationId
-                where p.Id = 1
-
-                TODO: Figure out query for Practitioner (should be almost identical)
-                TODO: Map result
-             */
-            var organizationQuery = from patient in _context.Patients
+            var relationshipQuery = from patient in _context.Patients
                 join relationship in _context.Relationships on patient.Id equals relationship.PatientId
                 join organization in _context.Organizations on relationship.Id equals organization.RelationshipId
                 join program in _context.OrganizationPrograms on organization.Id equals program.OrganizationId
                 join practitioner in _context.Practitioners on relationship.Id equals practitioner.RelationshipId
-                where patient.Eid.Equals(clinicalRelationshipRequest.PatientIdentifiers.Eid)
+                where relationship.Id.Equals(3)
                 select
                     new RelationshipDTO
                     {
+                        Id = relationship.Id,
                         Organization = new OrganizationDTO
                         {
                             Id = organization.Id,
@@ -116,20 +105,47 @@ namespace ProtoApp.GRPC
                     };
 
 
-            var foo = await organizationQuery.ToListAsync();
+            // All relationships
+            var relationships = await relationshipQuery.ToListAsync();
 
+            // Organizations
+            var organizations = relationships?.Select(x => x.Organization.ToGrpc() ?? new Organization())?.ToList();
+
+            // Practitioners
+            var practitioners = relationships?.Select(x => x.Practitioner.ToGrpc() ?? new Practitioner())?.ToList();
+
+            // Programs
+            var programs = relationships?.Select(x => x?.Program?.ToGrpc() ?? new CRISP.GRPC.ClinicalRelationship.Program())?.ToList();
+
+            // Merge the organizations
+            var mergedOrganizations =
+                from program in programs
+                join organization in organizations on program?.OrganizationId equals organization?.Id
+                select
+                    new Organization
+                    {
+                        Id = organization.Id,
+                        DataSource = organization.DataSource,
+                        Name = organization.Name,
+                        Source = organization.Source,
+                        SubstanceUseDisclosure = organization.SubstanceUseDisclosure,
+                        ContactInformation = organization.ContactInformation,
+                        Address = organization.Address,
+                        Programs = { program }
+                    };
             return new ClinicalRelationshipResponse
             {
                 PatientRelationships = new PatientRelationship
                 {
-                    Organizations = { },
-                    Practitioners = { }
+                    Organizations = { mergedOrganizations },
+                    Practitioners = { practitioners }
                 },
                 Error = null
             };
         }
     }
 
+    #region DataTransferObjects
     public class NameDTO
     {
         public string Firstname { get; set; }
@@ -137,14 +153,14 @@ namespace ProtoApp.GRPC
         public string MiddleName { get; set; }
         public string DisplayName { get; set; }
 
-        public Name ToGrpcName()
+        public Name ToGrpc()
         {
             return new Name
             {
-                Firstname = null,
-                MiddleName = null,
-                LastName = null,
-                DisplayName = null
+                Firstname = Firstname,
+                MiddleName = MiddleName,
+                LastName = LastName,
+                DisplayName = DisplayName
             };
         }
     }
@@ -155,17 +171,54 @@ namespace ProtoApp.GRPC
         public string Zip { get; set; }
         public string AddressLine1 { get; set; }
         public string AddressLine2 { get; set; }
+
+        public Address ToGrpc()
+        {
+            return new Address
+            {
+                City = City,
+                State = State,
+                Zip = Zip,
+                AddressLine1 = AddressLine1,
+                AddressLine2 = AddressLine2 ?? string.Empty
+            };
+        }
     }
     public class PhoneNumberDTO
     {
         public string Type { get; set; }
         public string Number { get; set; }
+
+        public PhoneNumber ToGrpc()
+        {
+            return new PhoneNumber
+            {
+                Number = Number,
+                Type = PhoneNumber.Types.PhoneType.Work // Need to map from Type
+            };
+        }
     }
     public class RelationshipDTO
     {
         public OrganizationDTO Organization { get; set; }
         public PractitionerDTO Practitioner { get; set; }
         public ProgramDTO Program { get; set; }
+        public long Id { get; set; }
+
+        public Relationship ToGrpc()
+        {
+            return new Relationship
+            {
+                Id = Id,
+                PatientId = null,
+                DataSource = null,
+                DateAdded = default,
+                DateUpdated = default,
+                Patient = null,
+                Organizations = null,
+                Practitioners = null
+            };
+        }
     }
     public class ProgramDTO
     {
@@ -173,6 +226,16 @@ namespace ProtoApp.GRPC
         public string Name { get; set; }
         public string Description { get; set; }
         public long? OrganizationId { get; set; }
+        public CRISP.GRPC.ClinicalRelationship.Program ToGrpc()
+        {
+            return new CRISP.GRPC.ClinicalRelationship.Program
+            {
+                Id = Id,
+                Name = Name,
+                Description = Description,
+                OrganizationId = OrganizationId ?? 0
+            };
+        }
     }
     public class PractitionerDTO
     {
@@ -182,7 +245,17 @@ namespace ProtoApp.GRPC
         public long? OrganizationId { get; set; }
         public AddressDTO Address { get; set; }
         public NameDTO Name { get; set; }
-
+        public Practitioner ToGrpc()
+        {
+            return new Practitioner
+            {
+                Id = Id,
+                Name = Name.ToGrpc(),
+                Address = Address.ToGrpc(),
+                ContactInformation = ContactInformation.ToGrpc(),
+                Type = Enum.TryParse<Practitioner.Types.ProviderType>(Type, out var type) ? type : Practitioner.Types.ProviderType.None
+            };
+        }
     }
     public class OrganizationDTO
     {
@@ -193,10 +266,33 @@ namespace ProtoApp.GRPC
         public bool? SubstanceUseDisclosure { get; set; }
         public ContactInformationDTO ContactInformation { get; set; }
         public AddressDTO Address { get; set; }
+        public Organization ToGrpc()
+        {
+            return new Organization
+            {
+                Id = Id,
+                DataSource = DataSource,
+                Name = Name,
+                Source = Source,
+                SubstanceUseDisclosure = SubstanceUseDisclosure ?? false,
+                ContactInformation = ContactInformation.ToGrpc(),
+                Address = Address.ToGrpc()
+            };
+        }
     }
     public class ContactInformationDTO
     {
         public PhoneNumberDTO Phone { get; set; }
         public string Email { get; set; }
+
+        public ContactInformation ToGrpc()
+        {
+            return new ContactInformation
+            {
+                Phone = Phone.ToGrpc(),
+                Email = Email
+            };
+        }
     }
+    #endregion
 }
